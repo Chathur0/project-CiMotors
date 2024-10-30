7baf853a56fcddfb330cc4c3787edfd85f3983a8
Chathur0 <145315287+Chathur0@users.noreply.github.com>
2024-10-30 19:00:35 +0530

    fixed message option

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,51 +1,36 @@
 const form = document.getElementById("form-submission");
-
-function sendEmail(name, email, subject, messageContent) {
-  Email.send({
-    Host: "smtp.elasticemail.com",
-    Username: "cimotors50@gmail.com",
-    Password: "39C5F44F91B2983DD7A60E0D3D59B422EA72",
-    To: "cimotors50@gmail.com",
-    From: "cimotors50@gmail.com",
-    Subject: subject,
-    Body: `
-            <h2>Contact Form Submission</h2>
-            <p><strong>Name:</strong> ${name}</p>
-            <p><strong>Email:</strong> ${email}</p>
-            <p><strong>Subject:</strong> ${subject}</p>
-            <p><strong>Message:</strong><br/> ${messageContent}</p>
-        `,
-  })
-    .then((message) => {
-      if (message == "OK") {
+const btn = document.getElementById("button");
+function sendEmail(params) {
+  btn.innerHTML = "Sending...";
+  emailjs.send("service_ygfa0gn", "template_w10etfh", params).then(
+    (res) => {
+      if (res.text == "OK") {
+        btn.innerHTML = "SEND MESSAGE";
         document.getElementById("message").innerHTML =
           "Thank You for Contacting Us!";
         document.getElementById("message").style.display = "block";
+        form.reset();
       } else {
+        btn.innerHTML = "SEND MESSAGE";
         document.getElementById("message").innerHTML =
           "We're sorry, but your message couldn't be sent. Please reach us directly at <a href='mailto:info@cimotors.co.nz'>info@cimotors.co.nz</a> or call us at 079291279.";
         document.getElementById("message").style.display = "block";
       }
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-      alert(
-        "Failure sending mail. Please check SMTP credentials and settings."
-      );
-    });
+    },
+    (err) => {
+      btn.innerHTML = "SEND MESSAGE";
+      alert(JSON.stringify(err));
+    }
+  );
 }
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const name = form.elements[0].value;
-  const email = form.elements[1].value;
-  const subject = form.elements[2].value;
-  const messageContent = form.elements[3].value;
+  var params = {
+    name: form.elements[0].value,
+    email_id: form.elements[1].value,
+    message: form.elements[2].value,
+  };
 
-  sendEmail(name, email, subject, messageContent).then(() => {
-    form.elements[0].value = ""; 
-    form.elements[1].value = ""; 
-    form.elements[2].value = ""; 
-    form.elements[3].value = ""; 
-  });
+  sendEmail(params);
 });
